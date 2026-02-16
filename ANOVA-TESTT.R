048b4cefd9f6763025d6fcb570db832330df1d30
Caliese <135731149+Caliese@users.noreply.github.com>
2026-02-16 14:44:14 -0500

    Add files via upload

---
+++ ANOVA-TESTT.R
@@ -0,0 +1,12 @@
+#Author: Caliese, Date: Feb 16, 2026, Purpose: test ANOVA
+
+#Load library dplyr
+library(dplyr)
+
+#Read the csv file in a variable
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
+
+#df <- read.csv(PATH) 
+
+#plot
+ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
